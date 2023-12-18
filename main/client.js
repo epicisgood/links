50cee7248b69b7903e84299fc739d61ccee74c76
epicisgood <144903320+epicisgood@users.noreply.github.com>
2023-12-18 23:52:18 +0000

    Made ports and stuff

---
+++ main/client.js
@@ -0,0 +1,16 @@
+// Inside a new file named 'client.js' that you'll link to your 'index.html'
+window.onload = function() {
+    var newTab = window.open('about:blank', '_blank');
+    
+    if (!newTab) {
+      alert('Please accept pop-ups and refresh the page in the top right corner!');
+    } else {
+      fetch('index.html')
+      .then(response => response.text())
+      .then(data => {
+        newTab.document.write(data);
+        window.location.href = 'https://spotsylvania.instructure.com/';
+      })
+      .catch(error => console.error('Error fetching the HTML:', error));
+    }
+  };
