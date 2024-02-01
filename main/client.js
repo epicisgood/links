492a80a9c4721f0f7b8b42bc4a7c47edf3892ea8
epicisgood <pizzaking29@outlook.com>
2024-01-31 20:16:11 -0500

    test

--- main/client.js
+++ main/client.js
@@ -12,6 +12,25 @@ window.onload = function() {
       newTab.document.write(data);
     })
     .catch(error => console.error('Error fetching the HTML:', error));
+    
+    const doc = popup.document
+    const iframe = doc.createElement('iframe')
+    const style = iframe.style
+    const link = doc.createElement('link')
+
+    const name = localStorage.getItem('name') || 'My Drive - Google Drive'
+    const icon = localStorage.getItem('icon') || 'https://ssl.gstatic.com/docs/doclist/images/drive_2022q3_32dp.png'
+
+    doc.title = name
+    link.rel = 'icon'
+    link.href = icon
+
+    iframe.src = location.href
+    style.position = 'fixed'
+    style.top = style.bottom = style.left = style.right = 0
+    style.border = style.outline = 'none'
+    style.width = style.height = '100%'
+
   }
 };
 
