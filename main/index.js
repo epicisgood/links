f4c1b2058524eca16c8aa9ef4278da2e002cd4a2
epicisgood <pizzaking29@outlook.com>
2024-04-01 21:08:28 -0400

    update

--- main/index.js
+++ main/index.js
@@ -27,12 +27,14 @@ app.post('/edpuzzle', async (req, res) => {
       throw new Error('Token and classId are required.');
     }
     const { token, classId } = req.body;
+    const encodedtoken = encodeURIComponent(token)
     const response = await axios.post('https://v2.schoolcheats.net/edpuzzle/assignments', {
       token,
       classId
     }, {
       headers: {
-        'Cookie': '_ga=GA1.1.2082089131.1711240477; _gcl_au=1.1.655362806.1711240477; token=' + encodeURIComponent(token) + '; __gads=ID=6a949120f3b84579:T=1711240478:RT=1711902748:S=ALNI_MbHZhKxGEiLcuJE-VWsy-z9hZXkmg; __gpi=UID=00000dd516633973:T=1711240478:RT=1711902748:S=ALNI_MakCljffK6VdSLlCkzPk8hBZmP9LQ; __eoi=ID=33320d6f248e4ae9:T=1711240478:RT=1711902748:S=AA-AfjYf1uKwpbMgET6R482nyN0s; FCNEC=%5B%5B%22AKsRol-YzDOYCuKrTEk599iWGJlG8JuJCfJfWdiDuBtZ-4cxRN4CdU_pnSYrXG_gLZLdjViiULS5Kyd_Z34XLtW5JF9xUZFy48vCeSq2W1XRHY9FFW6PTOZ-MGOGd6CJ2SokrSLCd4DOVJugYJKJrvrxdHTJnoSmdQ%3D%3D%22%5D%2Cnull%2C%5B%5B2%2C%22%5B%5B%5B%5B1%2Cnull%2C%5B1711839620%2C30988000%5D%2C10%5D%5D%5D%2C%5Bnull%2C26%2C%5B1711240481%2C469855000%5D%5D%5D%22%5D%5D%5D; _ga_K836MS1B0K=GS1.1.1711902746.6.1.1711902753.53.0.0'
+        'cookie': `_gcl_au=1.1.680293524.1711921265; _ga=GA1.1.278429596.1711921265; __gads=ID=6a949120f3b84579:T=1711240478:RT=1712018952:S=ALNI_MbHZhKxGEiLcuJE-VWsy-z9hZXkmg; __gpi=UID=00000dd516633973:T=1711240478:RT=1712018952:S=ALNI_MakCljffK6VdSLlCkzPk8hBZmP9LQ; __eoi=ID=33320d6f248e4ae9:T=1711240478:RT=1712018952:S=AA-AfjYf1uKwpbMgET6R482nyN0s; token=${token}; FCNEC=^%^5B^%^5B^%^22AKsRol_CfJkrwswR5lUCAaSkOXNRlb9nKUFO-tVOsI_govj0Dz7uF1RvOEj0nka_u2S-dzilmFo9AdnePR9aQcL0H6tOcX-aXpMd96JXu9JmnQ6Y4jQzvGttELtDJLFHe7G8vR03YsabsCIX7lgiaOMxwAVEfDlT0A^%^3D^%^3D^%^22^%^5D^%^2Cnull^%^2C^%^5B^%^5B2^%^2C^%^22^%^5B^%^5B^%^5B^%^5B1^%^2Cnull^%^2C^%^5B1711924913^%^2C809490000^%^5D^%^2C10^%^5D^%^5D^%^5D^%^2C^%^5Bnull^%^2C6^%^2C^%^5B1711980291^%^2C351215000^%^5D^%^5D^%^5D^%^22^%^5D^%^5D^%^5D; _ga_K836MS1B0K=GS1.1.1712018950.5.1.1712019000.10.0.0^`
+
       }
     });
     res.json(response.data);
