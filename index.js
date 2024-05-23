03411d943808f4aa16d7fcf1a68ae7a7c8359814
epicisgood <pizzaking29@outlook.com>
2024-05-22 20:13:59 -0400

    fr

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
-const express = require("express");
-const fs = require("fs");
-const axios = require("axios").default;
-const tough = require("tough-cookie");
-const { wrapper } = require("axios-cookiejar-support");
-const path = require("path");
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
+
+// Middleware to parse JSON and URL-encoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-require("dotenv").config();
 
-// Set up cookie jar globally
-const cookieJar = new tough.CookieJar();
-const client = wrapper(axios.create({ jar: cookieJar }));
+
+// Determine the directory name
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const mainPath = path.join(__dirname, "main");
 
+// Serve static files from the "main" directory
 app.use(express.static(mainPath));
 
 const routes = [
   { path: "/", file: "index.html" },
-  { path: "/answers", file: "edpuzzle.html" },
+  { path: "/schedule", file: "schedule.html" },
 ];
 
 routes.forEach((route) => {
@@ -34,74 +35,6 @@ routes.forEach((route) => {
 
 
 
-
-app.post("/edpuzzle-media", async (req, res) => {
-  const { token, classId } = req.body;
-
-  const csrfResponse = await client.get("https://edpuzzle.com/api/v3/csrf");
-  const EdpuzzleCSRFToken = csrfResponse.data.CSRFToken;
-
-  cookieJar.setCookieSync(`csrfToken=${EdpuzzleCSRFToken}`, "https://edpuzzle.com");
-
-
-  const class_response = await client.get(
-    `https://edpuzzle.com/api/v3/assignments/classrooms/${classId}/students?needle=`,
-    {
-      headers: {
-        Cookie: `token=${token};`
-},
-    }
-  );
-
-  const Media_ID = class_response.data.teacherAssignments[0].contentId;
-  console.log(Media_ID)
-
-  const media_response = await client.post(
-    `https://edpuzzle.com/api/v3/media/${Media_ID}`,
-    {
-      headers: {
-        Cookie: `token=${token};`,
-        "edpuzzleCSRF": EdpuzzleCSRFToken
-      },
-    }
-  );
-
-  res.json(media_response.data);
-
-  console.log(media_response.data);
-});
-
-
-
-
-
-app.post("/login", async (req, res) => {
-  try {
-    const { username, password } = req.body;
-
-    const csrfResponse = await client.get("https://edpuzzle.com/api/v3/csrf");
-    const EdpuzzleCSRFToken = csrfResponse.data.CSRFToken;
-
-    const response = await client.post(
-      "https://edpuzzle.com/api/v3/users/login",
-      {
-        username,
-        password,
-        role: "student",
-      },
-      {
-        headers: {
-          "x-csrf-token": EdpuzzleCSRFToken,
-        },
-      }
-    );
-    const token = response.headers;
-    res.json(token);
-  } catch (error) {
-    console.error("Error fetching data:", error.message);
-    res.status(500).json({ error: "An error occurred while fetching data." });
-  }
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- main/scripts/popup.js
+++ main/scripts/popup.js
@@ -30,7 +30,6 @@ if (!inFrame && !navigator.userAgent.includes('Firefox')) {
 
     doc.head.appendChild(link)
     doc.body.appendChild(iframe)
-    location.replace(localStorage.getItem('panicLink') || 'https://www.google.com/')
   }
 }
 
@@ -38,7 +37,7 @@ window.addEventListener('visibilitychange', () => {
   if (document.visibilityState === 'hidden')
     document.body.insertAdjacentHTML(
       'beforeend',
-      `<iframe src="/a/hvtrs8%2F-gmoelg.aoo" style="position:fixed;top:0;left:0;border:none;z-index:99999999999999999999999999;" height="100%" width="100%" allowfullscreen="" id="hider"></iframe>`
+      `<iframe style="position:fixed;top:0;left:0;border:none;z-index:99999999999999999999999999;" height="100%" width="100%" allowfullscreen="" id="hider"></iframe>`
     )
   else document.querySelector('#hider')?.remove()
 })
